b299e476b1ef5fcc7eba068e3a0fd6e82e8a8c88
Carlos Fornari <lilfornah@gmail.com>
2023-01-14 19:13:35 -0400

    stop car

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { SerialPort } = require('serialport')
 const readline = require('readline');
 
+let isMoving = false;
+
 const port = new SerialPort({
     path:'COM4',
     baudRate: 9600,
@@ -11,6 +13,7 @@ if (process.stdin.isTTY) process.stdin.setRawMode(true);
 
 process.stdin.on('keypress', (chunk, key) => {
     if(!key) return;
+    isMoving = true;
     switch(key.name){
         case 'up':
             return port.write([255,1]); //arriba
@@ -29,6 +32,13 @@ process.stdin.on('keypress', (chunk, key) => {
         case 'd' :
             return port.write([255,3]); //derecha
         case 'q':
-            return process.quit();
-    }
+            return process.exit();
+    } 
+    if(key.name == 'q') return process.exit();
+    setTimeout(() => {
+        isMoving = false;
+        setTimeout(() => {
+            if(!isMoving) console.log('stop')
+        },500)
+    }, 1);
 });
