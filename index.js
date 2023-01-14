ba71dd071efc0212f413981fe12e705b49c8f59e
Holeekey <danbrunosv@gmail.com>
2023-01-14 14:04:07 -0400

    Direction fixed

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ process.stdin.on('keypress', (chunk, key) => {
         case 'up':
             return port.write([255,1]); //arriba
         case 'down':
-            return port.write([1,255]); //abajo
+            return port.write([255,4]); //abajo
         case 'left':
-            return port.write([255,255]); //izquierda
+            return port.write([255,2]); //izquierda
         case 'right':
-            return port.write([1,1]); //derecha
+            return port.write([255,3]); //derecha
         case 'w' :
             return port.write([255,1]); //arriba
         case 's' :
-            return port.write([1,255]); //abajo
+            return port.write([255,4]); //abajo
         case 'a' :
-            return port.write([255,255]); //izquierda
+            return port.write([255,2]); //izquierda
         case 'd' :
-            return port.write([1,1]); //derecha
+            return port.write([255,3]); //derecha
         case 'q':
             return process.quit();
     }
